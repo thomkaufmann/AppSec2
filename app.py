3780592bf8366681b77e533f4fc11a05f1965b6b
Thomas <thom.kaufmann@gmail.com>
2019-10-13 19:59:05 +0200

    clean up forms and reset 2fa procedure

--- app.py
+++ app.py
@@ -115,7 +115,6 @@ def login():
          
          username = form.uname.data
          password = form.pword.data
-         pin = form.pin.data
          
          con = sql.connect("database.db")
          con.row_factory = sql.Row
@@ -126,13 +125,11 @@ def login():
          rows = cur.fetchall()
          con.close()
 
-         if len(rows) >= 1 and check_password_hash(rows[0]['password'],password) and rows[0]['pin'] == pin:
+         if len(rows) >= 1 and check_password_hash(rows[0]['password'],password):
             session['username'] = username
-            session['is_authenticated'] = True  
-            flash("Success: You are logged in!","result")
-            return redirect(url_for('spell_check'))   
-            # form = User2FAForm()
-            # form_type = '2FA'   
+            session['is_authenticated'] = False  
+            form = User2FAForm()
+            form_type = '2FA'   
          else:
             flash("Incorrect username or password. Please try again.","result")
          
@@ -164,7 +161,6 @@ class UserRegisterForm(FlaskForm):
 class UserLoginForm(FlaskForm):
    uname = StringField('Username', validators=[DataRequired(), Regexp(r'^[\w.@+-]+$')])
    pword = PasswordField('Password', validators=[DataRequired()])
-   pin = IntegerField('Two-Factor Authentication', validators=[DataRequired(), NumberRange(min=1000000000,max=9999999999)], id='2fa')
    submit = SubmitField('Submit')
 
 class User2FAForm(FlaskForm):
